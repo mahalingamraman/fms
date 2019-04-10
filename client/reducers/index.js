55150aaa6e41283c569395a789b4450250de5aa1
Anandhi <anandhi1598@gmail.com>
2019-04-10 15:10:11 +0530

    'finalLPTM'

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,12 +1,22 @@
 import { combineReducers } from 'redux';
-
-import githubReducer from './githubReducer';
 import loginReducer from './loginReducer';
+import homeReducer from './homeReducer';
+import peopleReducer from './peopleReducer';
+import qualityReducer from './qualityReducer';
+import velocityReducer from './velocityReducer';
+import velocitySMReducer from './velocitySMReducer';
+import costReducer from './costReducer';
+
+
 const rootReducer = combineReducers({
-	mahaData : githubReducer,
-	loginReducer: loginReducer,
-	
+    loginReducer: loginReducer,
+    homeReducer: homeReducer,
+    peopleReducer:peopleReducer,
+    qualityReducer:qualityReducer,
+    velocityReducer:velocityReducer,
+    velocitySMReducer:velocitySMReducer,
+    costReducer:costReducer
+   
 });
 
 export default rootReducer;
-
